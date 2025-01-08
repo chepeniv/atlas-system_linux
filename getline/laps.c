651807b0f1bac74f374713f6030f77d7057431af
chepeniv <jose.olmos@atlasschool.com>
2025-01-08 16:36:41 -0600

    added signature, working comments, and some #includes

---
+++ getline/laps.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "laps.h"
+
+void race_state(int *id, size_t size);
+
+/*
+ * task0 racing cars
+ * write a function which keeps track of laps made by each car in a race
+ *
+ * `id` (an array of ints) represents the identifier for each car
+ * `size` is the length of this array
+ *
+ * each identifier is expected to be unique
+ *
+ * if an identifier is unknown create a new car with laps = 0 and print
+ * "Car X joined the race\n"
+ *
+ * each time the function is called, the number of laps for each car in `id`
+ * must be incremented by 1. print the state of the race.
+ * "Race state:\n"
+ * sort cars by their identifier and print "Car X [Y laps]
+ *
+ * if the function is called with `size = 0` free all allocated memory
+ */

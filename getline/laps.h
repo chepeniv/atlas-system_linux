4760bba46b19d24de1d25445c71a625e777946dd
chepeniv <jose.olmos@atlasschool.com>
2025-01-11 16:28:57 -0600

    completed header file - no doc comments

--- getline/laps.h
+++ getline/laps.h
@@ -1,14 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>		/* read, write */
+#include <stdlib.h>		/* malloc, realloc, free */
+#include <string.h>		/* strcpy, strncpy, strdup, strcat; memset, memcpy */
+#include <stdio.h>		/* REMOVE AFTER TESTING */
 
-void print_int_array(int * numbers, int length)
-{
-	int i;
+#define LENGTH_OF(arr) sizeof(arr) / sizeof(*arr)
+#define MAX_CARS 128
 
-	putchar('[');
-	for (i = 0; i < length - 1; i++)
-		printf("%d, ", numbers[i]);
-	printf("%d", numbers[i]);
-	printf("] ");
+void init_array(int * zero_array, size_t length){
+	if (zero_array[0] == -1)
+		return;
+
+	for (size_t i = 0; i < length; i++)
+		zero_array[i] = -1;
 }
 
 void sort_array(int * arr, size_t length)
@@ -30,5 +33,35 @@ void sort_array(int * arr, size_t length)
 		}
 		len--;
 		pos = 0;
+	}
+}
+
+void check_participation(int * racers, int * lap_tracker, size_t number){
+	for (size_t i = 0; i < number; i++)
+	{
+		int car = racers[i];
+		if (lap_tracker[car] == -1)
+			printf("Car %d joined the race\n", car);
+	}
+}
+
+void race_state(int *racers, size_t number_of){
+	if (number_of == 0) {
+		return;
+	}
+
+	static int lap_tracker[MAX_CARS];
+	init_array(lap_tracker, MAX_CARS);
+
+	sort_array(racers, number_of);
+	check_participation(racers, lap_tracker, number_of);
+
+	printf("Race state:\n");
+	for (size_t i = 0; i < number_of; i++)
+	{
+		int car = racers[i];
+		lap_tracker[car] = lap_tracker[car] + 1;
+		int lap = lap_tracker[car];
+		printf( "Car %d [%d laps]\n", car, lap);
 	}
 }

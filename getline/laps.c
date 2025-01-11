65c858886a9405311d2af1338c2f2e3530d3a6cb
chepeniv <jose.olmos@atlasschool.com>
2025-01-11 17:58:31 -0600

    formated for betty compliance

--- getline/laps.c
+++ getline/laps.c
@@ -3,7 +3,8 @@
 #include <stdio.h>		/* printf */
 #include "laps.h"
 
-void init_array(int * zero_array, size_t length){
+void init_array(int *zero_array, size_t length)
+{
 	if (zero_array[0] == -1)
 		return;
 
@@ -11,7 +12,7 @@ void init_array(int * zero_array, size_t length){
 		zero_array[i] = -1;
 }
 
-void sort_array(int * arr, size_t length)
+void sort_array(int *arr, size_t length)
 {
 	size_t len = length, pos = 0;
 	int hold;
@@ -23,7 +24,7 @@ void sort_array(int * arr, size_t length)
 			if (arr[pos] > arr[pos + 1])
 			{
 				hold = arr[pos];
-				arr[pos] = arr[pos + 1] ;
+				arr[pos] = arr[pos + 1];
 				arr[pos + 1] = hold;
 			}
 			pos++;
@@ -33,21 +34,24 @@ void sort_array(int * arr, size_t length)
 	}
 }
 
-void check_participation(int * racers, int * lap_tracker, size_t number){
+void check_participation(int *racers, int *lap_tracker, size_t number)
+{
 	for (size_t i = 0; i < number; i++)
 	{
 		int car = racers[i];
+
 		if (lap_tracker[car] == -1)
 			printf("Car %d joined the race\n", car);
 	}
 }
 
-void race_state(int *racers, size_t number_of){
-	if (number_of == 0) {
+void race_state(int *racers, size_t number_of)
+{
+	if (number_of == 0)
 		return;
-	}
 
 	static int lap_tracker[MAX_CARS];
+
 	init_array(lap_tracker, MAX_CARS);
 
 	check_participation(racers, lap_tracker, number_of);
@@ -57,8 +61,11 @@ void race_state(int *racers, size_t number_of){
 	for (size_t i = 0; i < number_of; i++)
 	{
 		int car = racers[i];
+
 		lap_tracker[car] = lap_tracker[car] + 1;
+
 		int lap = lap_tracker[car];
-		printf( "Car %d [%d laps]\n", car, lap);
+
+		printf("Car %d [%d laps]\n", car, lap);
 	}
 }

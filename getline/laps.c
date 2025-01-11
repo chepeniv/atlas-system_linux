ca27883c997027ab7b95f98cfc7c71ca242fbbac
chepeniv <jose.olmos@atlasschool.com>
2025-01-11 11:46:40 -0600

    removed some work-comments and changed output strings

--- getline/laps.c
+++ getline/laps.c
@@ -5,44 +5,32 @@
 
 #define LENGTH_OF(arr) sizeof(arr) / sizeof(*arr)
 
-/*
- * keep track of the number of laps made by each car
- *
- * each identifier is expected to be unique
- *
- * if an identifier is unknown create a new car with laps = 0 and print
- * "Car X joined the race\n"
- *
- * each time the function is called, the number of laps for each car in `id`
- * must be incremented by 1. print the state of the race.
- * "Race state:\n"
- * sort cars by their identifier and print "Car X [Y laps]
- *
- * if size is 0 free all allocated memory
- */
 
 void race_state(int *racers, size_t number_of){
 	if (number_of == 0) {
+		/* free any allocated memory */
 		return;
 	}
 
 	static int lap_tracker[128] = {[0 ... 127] = -1};
 	sort_array(racers, number_of);
 
+	printf("Race state:\n");
+
 	for (size_t i = 0; i < number_of; i++)
 	{
-		int contestant = racers[i];
-		lap_tracker[contestant] = lap_tracker[contestant] + 1;
-		printf(
-			"contestant %d completed %d laps\n",
-			contestant,
-			lap_tracker[contestant]);
+		int car = racers[i];
+		lap_tracker[car] = lap_tracker[car] + 1;
+		int lap = lap_tracker[car];
+		if (lap == 0 )
+			printf("Car %d joined the race\n", car);
+		printf( "Car %d [%d laps]\n", car, lap);
 	}
 }
 
 int main(void) {
 	int car_ids[3] = {2, 5, 7};
-	int car_ids_2[5] = {10, 4, 3, 11, 44};
+	int car_ids_2[5] = {10, 4, 3, 11, 5};
 	int length = LENGTH_OF(car_ids);
 	int length_2 = LENGTH_OF(car_ids_2);
 
@@ -52,6 +40,9 @@ int main(void) {
 	race_state(car_ids_2, length_2);
 	race_state(car_ids, length);
 	race_state(car_ids, length);
+	race_state(car_ids, length);
+	race_state(car_ids, length);
+	race_state(car_ids_2, length_2);
 	race_state(car_ids_2, length_2);
 
 	return 0;

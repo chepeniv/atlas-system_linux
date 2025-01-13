32c14a73713e0e121d899ce6971bac55b3dc59a1
chepeniv <jose.olmos@atlasschool.com>
2025-01-12 20:26:47 -0600

    experimenting with assignment; saving progress

--- getline/_getline.c
+++ getline/_getline.c
@@ -10,6 +10,19 @@
  * if called with -1, free everything and reset all static variables
  */
 
+int set_line_end(char *buffer, int read_length)
+{
+	for (int i = 0; i < read_length; i++)
+	{
+		if (buffer[i] == '\n')
+		{
+			buffer[i] = '\0';
+			return (i);
+		}
+	}
+	return (read_length);
+}
+
 /**
  * _getline -  copy a READ_SIZE-length line from a given file descriptor in to
  * an internal buffer
@@ -19,23 +32,42 @@
  */
 char *_getline(const int file_desc)
 {
-	char *buffer = malloc(sizeof(char) * READ_SIZE);
-	int rlength = read(file_desc, buffer, READ_SIZE);
+	static int start_pos;
+	static int end_pos;
+	static int read_length;
+	static char *file_buffer;
+	static char *line_buffer;
 
-	if (rlength <= 0)
+	if (read_length == 0)
 	{
-		free(buffer);
+		file_buffer = malloc(sizeof(char) * READ_SIZE);
+		line_buffer = malloc(sizeof(char) * READ_SIZE * 2);
+	}
+
+	if (end_pos == 0)
+		read_length = read(file_desc, file_buffer, READ_SIZE);
+
+	if (read_length <= 0)
+	{
+		free(file_buffer);
+		free(line_buffer);
 		return (NULL);
 	}
 
-	for (int i = 0; i < READ_SIZE; i++)
+	end_pos += set_line_end(file_buffer, read_length);
+
+	strncpy(
+		&line_buffer[start_pos],
+		&file_buffer[start_pos],
+		end_pos - start_pos);
+
+	start_pos = end_pos + 1;
+
+	if (start_pos >= read_length)
 	{
-		if (buffer[i] == '\0' && buffer[i - 1] == '\n')
-		{
-			buffer[i - 1] = '\0';
-			break;
-		}
+		start_pos = 0;
+		end_pos =0;
 	}
 
-	return (buffer);
+	return (strdup(line_buffer));
 }
--- getline/_getline.h
+++ getline/_getline.h
@@ -2,7 +2,7 @@
 #define _GETLINE_
 
 /* the number of bytes to extract each iteration */
-#define READ_SIZE 256
+#define READ_SIZE 128
 
 char *_getline(const int file_desc);
 

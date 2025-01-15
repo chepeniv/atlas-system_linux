9dd66149dce257c719e8cbad98cc3e09ff0d5079
chepeniv <jose.olmos@atlasschool.com>
2025-01-15 02:15:35 -0600

    added doc comments in source, added func sigs in header

--- getline/_getline.c
+++ getline/_getline.c
@@ -5,15 +5,11 @@
 #include "_getline.h"
 
 /**
- * find_line - from a given buffer copy a single line into another buffer
- * starting from a given position
- * @file_buffer: the source buffer to analyse
- * @line_buffer: the destination buffer to write the extracted line into
- * @position: the position to from which to start reading the source buffer
- * @read_length: the length of the source buffer
+ * extract_line - read and find a line from a given data buffer
+ * @file: the struct containing the source buffer to analyse
  *
- * Return: void, values for line_buffer and position are written directly into
- * their corresponding buffers
+ * Return: a data structure containing the extracted line along with some
+ * metadata
  */
 data_buffer extract_line(data_buffer file)
 {
@@ -47,6 +43,15 @@ data_buffer extract_line(data_buffer file)
 	return (line);
 }
 
+/**
+ * extract_file_data - read all the data in a given file and store it in a
+ * buffer
+ * @file_desc: the file's reference id
+ * @file: the data structure containing the buffer to write to
+ *
+ * Return: returns a struct containing the data extracted as well as relevant
+ * metadata needed to properly process it
+ */
 data_buffer extract_file_data(int file_desc, data_buffer file)
 {
 	char *data_buffer = malloc(sizeof(char) * READ_SIZE);
@@ -54,6 +59,8 @@ data_buffer extract_file_data(int file_desc, data_buffer file)
 
 	file.data = malloc(sizeof(char));
 	file.length = 0;
+
+	/* incrementally extract all data from given file */
 	do {
 		memset(data_buffer, '\0', READ_SIZE);
 		read_length = read(file_desc, data_buffer, READ_SIZE);
@@ -62,6 +69,7 @@ data_buffer extract_file_data(int file_desc, data_buffer file)
 		{
 			file.data = realloc(file.data, file.length + read_length + 1);
 
+			/* append new data to data recorded */
 			strncpy(
 				&file.data[file.length],
 				data_buffer,
@@ -77,8 +85,8 @@ data_buffer extract_file_data(int file_desc, data_buffer file)
 }
 
 /**
- * _getline -  extract a line from the given file descriptor
- * @file_desc: the file id to parse
+ * _getline -  extract a line from the given file
+ * @file_desc: the file id to reference
  *
  * Return: a pointer to the line buffer or NULL upon eof or error
  */
@@ -92,6 +100,7 @@ char *_getline(const int file_desc)
 
 	line = extract_line(file);
 
+	/* terminate */
 	file.position = line.position;
 	if (file.position >= file.length)
 	{
--- getline/_getline.h
+++ getline/_getline.h
@@ -5,13 +5,21 @@
 /* #define READ_SIZE 1024 */
 #define READ_SIZE 2048
 
-typedef struct _data_buffer {
+/**
+ * struct _data_buffer - a structure for storing read data along with some metadata
+ * @length: how long the number of characters are stored within
+ * @position: where along the data was the last read point
+ * @data: a pointer to a buffer holding the data
+ */
+typedef struct _data_buffer
+{
 	int length;
 	int position;
 	char *data;
 } data_buffer;
 
 char *_getline(const int file_desc);
-/* void find_line(char *source, char *dest, int pos[1], int length); */
+data_buffer extract_line(data_buffer file);
+data_buffer extract_file_data(int file_desc, data_buffer file);
 
 #endif /* _GETLINE_ */

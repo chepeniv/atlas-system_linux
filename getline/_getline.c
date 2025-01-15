0407aedcc4a2a49333a7319f8027e9cbfa561378
chepeniv <jose.olmos@atlasschool.com>
2025-01-15 03:47:11 -0600

    added checks for malloc returns, renamed ambigous variable

--- getline/_getline.c
+++ getline/_getline.c
@@ -17,32 +17,51 @@ char *_getline(const int file_desc)
 
 	if (file.length < 0)
 	{
-		char *data_buffer = malloc(sizeof(char) * READ_SIZE);
+		char *chunk = malloc(sizeof(char) * READ_SIZE);
 		int read_length = 0;
 
 		file.data = malloc(sizeof(char));
-		file.length = 0;
+		if (!file.data || !chunk)
+		{
+			free(chunk);
+			free(file.data);
+			return (NULL);
+		}
 
 		/* incrementally extract all data from given file */
+		file.length = 0;
 		do {
-			memset(data_buffer, '\0', READ_SIZE);
-			read_length = read(file_desc, data_buffer, READ_SIZE);
+			memset(chunk, '\0', READ_SIZE);
+			read_length = read(file_desc, chunk, READ_SIZE);
 
 			if (read_length > 0)
 			{
 				file.data = realloc(file.data, file.length + read_length + 1);
+				if (!file.data)
+				{
+					free(chunk);
+					free(file.data);
+					return (NULL);
+				}
 
 				/* append new data to data recorded */
 				strncpy(
 						&file.data[file.length],
-						data_buffer,
+						chunk,
 						read_length);
 
 				file.length += read_length;
 			}
 		} while (read_length);
 
-		free(data_buffer);
+		free(chunk);
+	}
+
+	/* abandon empty file */
+	if (file.length <= 1)
+	{
+		free(file.data);
+		return (NULL);
 	}
 
 	/* move pass newline char */
@@ -60,6 +79,13 @@ char *_getline(const int file_desc)
 	/* allocate needed memory for line data */
 	line.length = line.position - file.position;
 	line.data = malloc(sizeof(char) * (line.length + 1));
+	if (!line.data)
+	{
+		free(line.data);
+		free(file.data);
+		return (NULL);
+	}
+
 	memset(line.data, '\0', line.length + 1);
 
 	/* copy line data from file_buffer into line_buffer */

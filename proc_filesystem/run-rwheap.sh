6fcb53303ba3f22bfd72a64c16e6c37e80076730
chepeniv <jose.olmos@atlasschool.com>
2025-02-23 21:24:20 -0600

    debugged run_ script

--- proc_filesystem/run-rwheap.sh
+++ proc_filesystem/run-rwheap.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 INFECHO_PID="$(pidof inf_echo.x)";
-SEARCH_STR= "hello, world!!!!";
-REPLACE_STR="goodbye, friend!";
+SEARCH_STR="hello, world!";
+REPLACE_STR="bye, friends!";
 
 if [[ -v INFECHO_PID ]];
 then
 	echo pid found: $INFECHO_PID;
-	python3 read_write_heap.py $INFECHO_PID "$SEARCH_STR" "$REPLACE_STR" 2>1;
+	./read_write_heap.py $INFECHO_PID "$SEARCH_STR" "$REPLACE_STR" 2>1;
 else
 	echo no such process currently running;
 fi

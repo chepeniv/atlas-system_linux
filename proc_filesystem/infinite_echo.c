dd3578d62e2f8a8a995272aed365cb71b8be2fc5
chepeniv <jose.olmos@atlasschool.com>
2025-02-23 20:38:01 -0600

    edited target program to manage it's own mem

--- proc_filesystem/infinite_echo.c
+++ proc_filesystem/infinite_echo.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int c, char **argv)
 {
+	char *echo_txt;
+	int len;
+
 	if (c < 2)
 	{
 		printf("usage: ./inf_echo \"string\"\n");
 		return (1);
 	}
 
+	len = strlen(argv[1]);
+	echo_txt = malloc(sizeof(char) * len);
+	strcpy(echo_txt, argv[1]);
+
 	while (1)
 	{
-		printf("%s\n", argv[1]);
+		printf("%s\n", echo_txt);
 		sleep(2);
 	}
+
+	free(echo_txt);
 
 	return(0);
 }

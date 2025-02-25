72f0f8f5fd30d6bc95405e1ab5f6029c85711f5c
chepeniv <jose.olmos@atlasschool.com>
2025-02-25 07:13:44 -0600

    added lenth checks and handling, updated comments

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -3,11 +3,8 @@
 definition and execution of a heap data overwrite program
 '''
 
-# determine if the given pid is valid
-# if the string is found overwrite it with a new string
-# if the string is not found do nothing
 
-def overwrite_heap(argv)
+def overwrite_heap(argv):
     '''
     analyzes the heap of a given process pid currently running and overwrites
     the target string within the heap with the source string data
@@ -16,15 +13,11 @@ def overwrite_heap(argv)
     # HOLBERTON CHECKER:
     #
     # case: normal replace
-    # case: not found
-    # case: short replace
     # case: replace by shortest string ??
-    # case: replace by empty string
-    #
-    # pep8 validation
+    # determine if the given pid is valid
+    # if the string is not found do nothing
 
     if (len(argv) < 4):
-        print("{}: insuficient arguments given".format(argv[0]))
         print("usage: `read_write_heap.py <pid> <search> <target>`")
         exit(1)
 
@@ -32,6 +25,12 @@ def overwrite_heap(argv)
     source = bytes(argv[3], "ASCII")
     len_target = len(target)
     len_source = len(source)
+
+    if len_target < len_source:
+        source = source[:len_target]
+    if len_target > len_source:
+        len_target -= len_source
+        source += bytes(len_target)
 
     pid_dir = f"/proc/{argv[1]}/"
     mapfile = open(pid_dir + "maps")
@@ -50,11 +49,15 @@ def overwrite_heap(argv)
     memfile.seek(addr_low)
 
     heap_data = memfile.read(addr_high - addr_low)
-    pos = heap_data.index(target)
+    pos = heap_data.find(target)
+    if pos < 0:
+        memfile.close()
+        return 1
 
     memfile.seek(addr_low + pos, 0)
     memfile.write(source)
     memfile.close()
+
 
 if __name__ == "__main__":
     from sys import argv

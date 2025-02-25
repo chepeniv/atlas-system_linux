5067fb89917aa3b6c35c60de6bc8adf045fdea5f
chepeniv <jose.olmos@atlasschool.com>
2025-02-25 04:26:36 -0600

    updated gitignores, created placeholders, added work comments

---
+++ readelf/0-hreadelf.c
@@ -0,0 +1,39 @@
+/*
+ * emulate: readelf -W -h
+ * usage : ./0-hreadelf elf_flie
+ *
+ * display information contained in the ELF file header
+ * handle various operating systems and architectures
+ * stdout, stderr, and status should be exactly the same
+ */
+
+/*
+ * ELF Header:
+ *   Magic:   7f 45 4c 46 02 01 01 00 00 00 00 00 00 00 00 00
+ *   Class:                             ELF64
+ *   Data:                              2's complement, little endian
+ *   Version:                           1 (current)
+ *   OS/ABI:                            UNIX - System V
+ *   ABI Version:                       0
+ *   Type:                              EXEC (Executable file)
+ *   Machine:                           Advanced Micro Devices X86-64
+ *   Version:                           0x1
+ *   Entry point address:               0x400600
+ *   Start of program headers:          64 (bytes into file)
+ *   Start of section headers:          6936 (bytes into file)
+ *   Flags:                             0x0
+ *   Size of this header:               64 (bytes)
+ *   Size of program headers:           56 (bytes)
+ *   Number of program headers:         9
+ *   Size of section headers:           64 (bytes)
+ *   Number of section headers:         31
+ *   Section header string table index: 28
+ */
+
+/*
+ * errors:
+ *
+ * "readelf: Error: 'doesntexist': No such file"
+ * "readelf: Error: Not an ELF file - it has the wrong magic bytes at the start"
+ */
+

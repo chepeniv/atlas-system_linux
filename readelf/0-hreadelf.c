7ede8202db7211b9c4090f5db5d4d03b2c273509
chepeniv <jose.olmos@atlasschool.com>
2025-02-26 03:26:30 -0600

    updated the main with new func signs

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -5,6 +5,7 @@
 #include "headers/print.h"
 #include "headers/const.h"
 #include "headers/parse.h"
+#include "headers/data.h"
 #include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -60,43 +61,37 @@ int main(int c, char **argv)
 	int readlen, hlen = 64;
 	FILE *elf_file;
 	char *procname, *filename;
-	unsigned char *elfdata;
+	unsigned char *elf_raw;
 
 	procname = argv[0];
 	if (c == 1)
-	{
-		errno = W_NOARG;
-		err_print(procname, NULL);
-	}
+		err_print(W_NOARG, procname, NULL);
 
 	filename = argv[1];
 	elf_file = fopen(filename, "rb");
 	if (!elf_file)
-		err_print(procname, filename);
+		err_print(errno, procname, filename);
 
-	mem_alloc((void **) &elfdata, BYTES, hlen);
-	readlen = fread(elfdata, BYTES, hlen, elf_file);
+	mem_alloc((void **) &elf_raw, BYTES, hlen);
+	readlen = fread(elf_raw, BYTES, hlen, elf_file);
 	if (readlen < 0)
 	{
-		free(elfdata);
-		err_print(procname, filename);
+		free(elf_raw);
+		err_print(errno, procname, filename);
 	}
 
-	/*
-	 * parse header bytes
-	 * interpret header data
-	 */
-
-	if (is_elf(elfdata))
-		print_hex(elfdata, hlen);
+	if (is_elf(elf_raw))
+	{
+		/* process raw elf data here */
+		print_hex(elf_raw, hlen);
+	}
 	else
 	{
-		free(elfdata);
-		errno = E_NELF;
-		err_print(procname, filename);
+		free(elf_raw);
+		err_print(E_NELF, procname, filename);
 	}
 
-	free(elfdata);
+	free(elf_raw);
 
 	return (EXIT_SUCCESS);
 }

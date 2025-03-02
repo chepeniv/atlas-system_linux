31c7376b6ce2183724529c45a161606dedd7d616
chepeniv <jose.olmos@atlasschool.com>
2025-03-02 10:41:26 -0600

    added new parse util funcs

--- readelf/defs/parse_utils.c
+++ readelf/defs/parse_utils.c
@@ -4,18 +4,66 @@
 #include "../headers/parse_utils.h"
 #include "../headers/mem.h"
 
-void *get_bytes(const unsigned char *data, int pos, int incr)
+uint64_t bitwise_reverse(uint64_t origin, uint64_t len)
 {
-	int arch_class = 0x04;
-	long *value;
+	uint64_t l_pos, r_pos, shift, rev = 0;
 
-	if (data[arch_class] == ELFCLASS32)
+	for (uint64_t b = 0; b < len / 2u; b++)
+	{
+		shift = ((len - 1u) - (2u * b)) * 8u;
+		r_pos = 0xffu << (b * 8u);
+		l_pos = r_pos << shift;
+		rev |= (r_pos & origin) << shift;
+		rev |= (l_pos & origin) >> shift;
+	}
+
+	return (rev);
+}
+
+int is_elf(const unsigned char *data)
+{
+	unsigned char elfmagic[4] = { 0x7f, 0x45, 0x4c, 0x46 };
+
+	for (int i = 0; i < 4; i++)
+		if (elfmagic[i] != data[i])
+			return (0);
+
+	return (1);
+}
+
+int get_endian_shift(const unsigned char *data)
+{
+	if (data[EI_DATA] == ELFDATA2MSB)
+		return (1);
+	return (0);
+}
+
+void *get_bytes(const unsigned char *data, int pos, int incr, int typesize)
+{
+	void *value;
+	int shift = 0;
+
+	(void) typesize;
+
+	if (data[EI_CLASS] == ELFCLASS32)
 		value = (void *) &data[pos];
 	else
 		value = (void *) &data[pos + incr]; /* ELFCLASS64 */
 
-
 	return (value);
+}
+
+char *make_uint16_text(
+const unsigned char *data,
+int pos, int incr, char *append)
+{
+	uint16_t *num;
+	char *mailback;
+
+	num = get_bytes(data, pos, incr, sizeof(uint16_t));
+	mailback = create_text__int_str(*num, append, 32);
+
+	return (mailback);
 }
 
 char *get_hex_str(const unsigned char *data, int pos, int incr, int bytes)
@@ -43,23 +91,4 @@ char *create_text__int_str(long value, char *append, int limit)
 		strcat(mailback, append);
 
 	return (mailback);
-}
-
-int is_elf(const unsigned char *data)
-{
-	unsigned char elfmagic[4] = { 0x7f, 0x45, 0x4c, 0x46 };
-
-	for (int i = 0; i < 4; i++)
-		if (elfmagic[i] != data[i])
-			return (0);
-
-	return (1);
-}
-
-int is_arch_32(const unsigned char *data)
-{
-	if (data[0x04] == 1)
-		return (1);
-
-	return (0);
 }
--- readelf/headers/parse_utils.h
+++ readelf/headers/parse_utils.h
@@ -1,10 +1,14 @@
 #ifndef _PARSE_UTILS_
 #define _PARSE_UTILS_
 
+typedef unsigned long ulong;
+
 int is_elf(const unsigned char *data);
-int is_arch_32(const unsigned char *data);
-void *get_bytes(const unsigned char *data, int pos, int incr);
+void *get_bytes(const unsigned char *data, int pos, int incr, int typesize);
 char *get_hex_str(const unsigned char *data, int pos, int incr, int bytes);
 char *create_text__int_str(long value, char *append, int limit);
+int get_endian_shift(const unsigned char *data);
+char *make_uint16_text(
+	const unsigned char *data, int pos, int incr, char *append);
 
 #endif /* _PARSE_UTILS_ */

6c28e3829f69cc4144dc40926f1cf5c2b7647533
chepeniv <jose.olmos@atlasschool.com>
2025-03-15 14:28:15 -0500

    added 'supplementary funcs' section to header

--- signals/signals.h
+++ signals/signals.h
@@ -3,11 +3,17 @@
 
 #include <sys/types.h>
 
+/* REQUIRED FUNCTIONS */
+
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
 int pid_exist(pid_t pid);
+
+/* SUPPLEMENTARY FUNCTIONS */
+
+void print_gotcha(int signum);
 
 #endif /* _SIGS_ */

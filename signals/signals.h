59635b3ce428eba7ddde39070e193112a4d56e34
chepeniv <jose.olmos@atlasschool.com>
2025-03-15 11:28:22 -0500

    added requiered func decl to header files

---
+++ signals/signals.h
@@ -0,0 +1,13 @@
+#ifndef _SIGS_
+#define _SIGS_
+
+#include <sys/types.h>
+
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+int pid_exist(pid_t pid);
+
+#endif /* _SIGS_ */

d1d9e7251ba986c005efe9ce9664ea2371a4d296
chepeniv <jose.olmos@atlasschool.com>
2025-03-16 08:28:08 -0500

    prep handle_sigaction def file

--- signals/2-handle_sigaction.c
+++ signals/2-handle_sigaction.c
@@ -1,13 +1,31 @@
 #include "signals.h"
+#include <stdio.h>
 
 /**
- * function_name - brief description
- * @paramA: desc of paramA
- * @paramB: desc of paramB
- * ...
+ * print_gotcha - prints gotcha message along with value received
+ * @signum: number to print with message
  *
- * Description: a more thorough desc
- * sub section: sub sect desc
- *
- * Return: desc of return vals
+ * Description: satisfies the 'void (*sighandler_t)(int)' form needed to be
+ * passed to the signal() call
  */
+
+void print_gotcha(int signum)
+{
+	printf("Gotcha! %d\n", signum);
+}
+
+/**
+ * handle_sigaction - sets a new  handler for the `SIGINT` signal
+ *
+ * Description: - the handler prints 'Gotcha! [<signum>]\n' every time `Ctrl-C`
+ * is pressed
+ * restrictions: 'signal'(2) is not allowed
+ *
+ * Return: on success '0' ; on error '-1'
+ */
+
+int handle_sigaction(void)
+{
+	return (0);
+}
+

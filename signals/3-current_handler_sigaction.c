311ff9cef9f9495b5e936cdb552aa28764ae1669
chepeniv <jose.olmos@atlasschool.com>
2025-03-16 11:36:12 -0500

    init implemention of current_handler_sigaction()

--- signals/3-current_handler_sigaction.c
+++ signals/3-current_handler_sigaction.c
@@ -1,13 +1,35 @@
 #include "signals.h"
+#include <signal.h>
+#include <stdlib.h>
 
 /**
- * function_name - brief description
- * @paramA: desc of paramA
- * @paramB: desc of paramB
- * ...
+ * current_handler_signal - retrieve the current handler for the 'SIGINT'
+ * signal
  *
- * Description: a more thorough desc
- * sub section: sub sect desc
+ * Description: the handler remains unchanged after calling this function
+ * restrictions: 'sigaction'(2) must be used; 'signal'(2) is not allowed
  *
- * Return: desc of return vals
+ * Return: return a pointer to the current handler of 'SIGINT' or 'NULL' upon
+ * failure
  */
+
+void (*current_handler_sigaction(void))(int)
+{
+	void (*current_handler)(int);
+	struct sigaction current_sa;
+	struct sigaction probe_sa;
+
+	probe_sa.sa_handler = SIG_DFL;
+
+	sigaction(SIGINT, &probe_sa, &current_sa);
+	current_handler = current_sa.sa_handler;
+
+	if (current_handler == SIG_ERR)
+		return (NULL);
+
+	if (sigaction(SIGINT, &current_sa, NULL) == -1)
+		return (NULL);
+
+	return (current_handler);
+
+}

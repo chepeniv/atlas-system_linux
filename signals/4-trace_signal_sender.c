a29db1176ae069b04331ac29c361c3c6dcf0e57d
chepeniv <jose.olmos@atlasschool.com>
2025-03-16 14:13:45 -0500

    init implementation of trace_signal_sender

--- signals/4-trace_signal_sender.c
+++ signals/4-trace_signal_sender.c
@@ -1,13 +1,45 @@
 #include "signals.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * function_name - brief description
- * @paramA: desc of paramA
- * @paramB: desc of paramB
- * ...
- *
- * Description: a more thorough desc
- * sub section: sub sect desc
- *
- * Return: desc of return vals
+ * handler_print_sender - prints 'SIGQUIT sent by <pid>\n' each time 'SIGQUIT'
+ * and only 'SIGQUIT' is caught
+ * @sig_num: the signal number caught
+ * @sig_info: metadata about the signal caught
+ * @sig_context: ucontext
  */
+
+void sig_handler_print_sender(
+int        sig_num,
+siginfo_t *sig_info,
+void      *sig_context)
+{
+	(void) sig_num;
+	(void) sig_context;
+
+	printf("SIGQUIT sent by %d\n", sig_info->si_pid);
+}
+
+/**
+ * trace_signal_sender - sets up a handler for the `SIGQUIT` signal
+ *
+ * Description: the handler prints 'SIGQUIT sent by <pid>\n' each time
+ * 'SIGQUIT' and only 'SIGQUIT' is caught
+ *
+ * Return: on success '0'; on error '-1'
+ */
+
+int trace_signal_sender(void)
+{
+	int result;
+	struct sigaction new_sa;
+
+	new_sa.sa_sigaction = &sig_handler_print_sender;
+	new_sa.sa_flags = SA_SIGINFO;
+
+	result = sigaction(SIGQUIT, &new_sa, NULL);
+
+	return (result);
+}
--- signals/signals.h
+++ signals/signals.h
@@ -2,6 +2,7 @@
 #define _SIGS_
 
 #include <sys/types.h>
+#include <signal.h>
 
 /* REQUIRED FUNCTIONS */
 
@@ -15,5 +16,9 @@ int pid_exist(pid_t pid);
 /* SUPPLEMENTARY FUNCTIONS */
 
 void print_gotcha(int signum);
+void sig_handler_print_sender(
+	int        sig_num,
+	siginfo_t *sig_info,
+	void      *sig_context);
 
 #endif /* _SIGS_ */

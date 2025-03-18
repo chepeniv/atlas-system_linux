31ed0c907572728638f9acf61c2a79b80efa9b35
chepeniv <jose.olmos@atlasschool.com>
2025-03-18 13:50:03 -0500

    added preliminary include directives and doc comment templates

---
+++ signals/101-sigset_init.c
@@ -0,0 +1,13 @@
+#include <signal.h>
+#include <sys/types.h>
+#include "signals.h"
+
+/**
+ * func - desc
+ * @param: desc
+ *
+ * Description: details
+ * sub section: extra info
+ *
+ * Return: desc of return val and meanings
+ */

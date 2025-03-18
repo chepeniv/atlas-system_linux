25dea360f4bd87edfc79e86f348cd92008332cb5
chepeniv <jose.olmos@atlasschool.com>
2025-03-17 20:30:30 -0500

    init implementation of signal_send script

---
+++ signals/8-signal_send.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+
+# INSTRUCTIONS
+# write a 'sh' script that sends the 'SIGQUIT' signal to a process given by
+# its PID
+#
+# usage: './signal_send.sh <pid>'
+# '<pid>' will not be '0'
+#
+# if an incorrect number of arguments is given, print 'Usage: %s <pid>\n'
+#     where '%s' is given by 'argv[0]'
+#     exit with '1'
+
+if [ $# != 1 ];
+	then printf "Usage: %s <pid>\n" $0;
+	return 1;
+else
+	# echo "SIGQUIT"
+	kill -3 $1
+fi

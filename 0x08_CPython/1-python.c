b4e7296fd31a43ed850c5628026de07af952ee74
chepeniv <jose.olmos@atlasschool.com>
2025-03-30 18:08:06 -0500

    init print_python_list_items()

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -1,8 +1,27 @@
 #include <Python.h>
 
 /**
+ * print_python_list_items - prints out each elements position and type
+ * @pylist: python list to iterate over
+ * @list_len: the length of the list given
+ */
+
+void print_python_list_items(PyObject *pylist, Py_ssize_t list_len)
+{
+	PyTypeObject *type;
+	PyObject *item;
+
+	for (Py_ssize_t i = 0; i < list_len; i++)
+	{
+		item = PyList_GetItem(pylist, i);
+		type = Py_TYPE(item);
+		printf("Element %ld: %s\n", i, type->tp_name);
+	}
+}
+
+/**
  * print_python_list - printout basic information about give python list object
- * @pylist: python list to analyze
+ * @pylist: python object to analyze
  *
  * output:
  * [*] Python list info
@@ -15,9 +34,20 @@
 
 void print_python_list(PyObject *pylist)
 {
+	Py_ssize_t list_size, list_alloc;
+
 	if (PyList_Check(pylist))
 	{
-		printf("is list");
+		list_size = PyList_Size(pylist);
+		list_alloc = list_size;
+
+		printf(
+			"[*] Python list info\n"
+			"[*] Size of the Python List = %ld\n"
+			"[*] Allocated = %ld\n",
+			list_size, list_alloc
+			);
+		print_python_list_items(pylist, list_size);
 	}
 	else
 	{

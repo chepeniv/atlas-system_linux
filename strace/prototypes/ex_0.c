f58982b314f5fcf75217a7b746a9833c37f89c7b
chepeniv <jose.olmos@atlasschool.com>
2025-09-08 03:27:47 -0500

    added working comments to main() of ex_0.c

--- strace/prototypes/ex_0.c
+++ strace/prototypes/ex_0.c
@@ -2,6 +2,30 @@
 
 int main(void)
 {
+	//// PHASE 1 ////
+	//
+	// parse arguments
+	// call binary
+	//
+	//// PHASE 2 ////
+	//
+	// parse arguments
+	// fork
+	// if child:
+	//     call ptrace with PTRACE_TRACEME
+	//     use execve to execute given binary
+	// if parent:
+	//     wait for child
+	//     loop
+	//         call ptrace with request PTRACE_SINGLESTEP
+	//         print 'single step' each iteration
+	//     when done print child's exit status
+	//
+	//// NOTE ////
+	///
+	// PTRACE_SINGLESTEP suspends the tracee every time the register 'ip'
+	// changes
+
 	printf("hello, world!\n");
 	return 0;
 }

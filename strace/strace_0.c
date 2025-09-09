15ddd9e97cc18ac74cd9b86746788edb7bf57979
chepeniv <jose.olmos@atlasschool.com>
2025-09-09 16:13:48 -0500

    moved variable to stack, forcefully skipped first syscall

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -10,7 +10,7 @@ int
 main(int count, char **args)
 {
 	pid_t parent = 0;
-	int *wstatus = 0, syscall_enter = 0;
+	int wstatus = 0, syscall_enter = 0, first_syscall = 1;
 	struct user_regs_struct *syscall_regs;
 
 	if (count <= 1)
@@ -22,24 +22,23 @@ main(int count, char **args)
 	parent = fork();
 	if (parent)
 	{
-		wstatus = malloc(sizeof(int));
 		syscall_regs = malloc(sizeof(struct user_regs_struct));
 
-		wait(wstatus);
+		wait(&wstatus);
 		do {
 			ptrace(PTRACE_SYSCALL, parent, NULL, NULL);
-			wait(wstatus);
+			wait(&wstatus);
 
-			if (syscall_enter && WIFSTOPPED(*wstatus) && (WSTOPSIG(*wstatus) != SIGALRM))
+			if (syscall_enter && !first_syscall)
 			{
 				ptrace(PTRACE_GETREGS, parent, NULL, syscall_regs);
 				fprintf(stderr, "%llu\n", syscall_regs->orig_rax);
 			}
 			syscall_enter = !syscall_enter;
-		} while (!WIFEXITED(*wstatus));
+			first_syscall = 0;
+		} while (!WIFEXITED(wstatus));
 
 		free(syscall_regs);
-		free(wstatus);
 	}
 	else
 	{

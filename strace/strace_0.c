46c53d46a37694501029dee23c10007a6e28f598
chepeniv <jose.olmos@atlasschool.com>
2025-09-09 13:35:28 -0500

    updated makefile, .clang-format, created strace_0.c

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -1,7 +1,50 @@
-#include <stdio.h>
+#include <signal.h>
+#include <stdio.h>  /* printf */
+#include <stdlib.h> /* malloc */
+#include <sys/ptrace.h>
+#include <sys/user.h> /* user_regs_struct */
+#include <sys/wait.h>
+#include <unistd.h> /* fork */
 
-int main(void)
+int
+main(int count, char **args)
 {
-	printf("hello, world!\n");
-	return 0;
+	pid_t parent = 0;
+	int *wstatus = 0, syscall_enter = 0;
+	struct user_regs_struct *syscall_regs;
+
+	if (count <= 1)
+	{
+		printf("no command provided\n");
+		return (1);
+	}
+
+	parent = fork();
+	if (parent)
+	{
+		wstatus = malloc(sizeof(int));
+		syscall_regs = malloc(sizeof(struct user_regs_struct));
+
+		do {
+			if (syscall_enter)
+			{
+				ptrace(PTRACE_GETREGS, parent, 0, syscall_regs);
+				printf("%llu\n", syscall_regs->orig_rax);
+			}
+			ptrace(PTRACE_SYSCALL, parent, 0, 0);
+			syscall_enter = !syscall_enter;
+			wait(wstatus);
+		} while (!WIFEXITED(*wstatus));
+
+		free(syscall_regs);
+		free(wstatus);
+	}
+	else
+	{
+		ptrace(PTRACE_TRACEME, 0, 0, 0);
+		raise(SIGSTOP);
+		execve(args[1], &args[1], NULL);
+	}
+
+	return (0);
 }

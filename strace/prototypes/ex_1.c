7559f5e15fb218403abf8023ac1cb4686f51551f
chepeniv <jose.olmos@atlasschool.com>
2025-09-09 06:29:14 -0500

    changed indentation formatting to spaces over tabs

--- strace/prototypes/ex_1.c
+++ strace/prototypes/ex_1.c
@@ -1,38 +1,42 @@
 #include <signal.h>
-#include <stdio.h>      /* printf */
-#include <stdlib.h>     /* malloc */
-#include <sys/wait.h>
-#include <unistd.h>     /* fork */
+#include <stdio.h>  /* printf */
+#include <stdlib.h> /* malloc */
 #include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <unistd.h> /* fork */
 
-int main(int count, char **args)
+int
+main (int count, char **args)
 {
-	pid_t parent = 0;
-	int *wstatus = 0;
+    pid_t parent = 0;
+    int *wstatus = 0;
 
-	if (count <= 1)
-	{
-		printf("no command provided\n");
-		return 1;
-	}
+    if (count <= 1)
+    {
+        printf ("no command provided\n");
+        return 1;
+    }
 
-	parent = fork();
-	if (parent) {
-		wstatus = malloc(sizeof(int));
+    parent = fork ();
+    if (parent)
+    {
+        wstatus = malloc (sizeof (int));
 
-		while (wait(wstatus) > -1)
-		{
-			printf("single step\n");
-			ptrace(PTRACE_SINGLESTEP, parent, 0, 0);
-		}
+        while (wait (wstatus) > -1)
+        {
+            printf ("single step\n");
+            ptrace (PTRACE_SINGLESTEP, parent, 0, 0);
+        }
 
-		printf("Exit status: %d\n", *wstatus);
-		free(wstatus);
-	} else {
-		ptrace(PTRACE_TRACEME, 0, 0, 0);
-		raise(SIGSTOP);
-		execve(args[1], &args[1], NULL);
-	}
+        printf ("Exit status: %d\n", *wstatus);
+        free (wstatus);
+    }
+    else
+    {
+        ptrace (PTRACE_TRACEME, 0, 0, 0);
+        raise (SIGSTOP);
+        execve (args[1], &args[1], NULL);
+    }
 
-	return 0;
+    return 0;
 }

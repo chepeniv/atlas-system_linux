fc33284484c0ec05a168061729e85766a3be6bd6
chepeniv <jose.olmos@atlasschool.com>
2025-09-10 15:00:08 -0500

    created strace_1.c, updated Makefile

---
+++ strace/strace_1.c
@@ -0,0 +1,62 @@
+#include "include/syscalls.h"
+#include <signal.h>
+#include <stdio.h>  /* printf */
+#include <stdlib.h> /* malloc */
+#include <sys/ptrace.h>
+#include <sys/user.h> /* user_regs_struct */
+#include <sys/wait.h>
+#include <unistd.h> /* fork */
+
+// for every intercepted system call, print its name followed by a newline
+// no need to handle the `PATH`: `command` will be given as a full path to a
+// binary
+
+int
+main(int count, char **args)
+{
+	pid_t parent = 0;
+	int wstatus = 0, syscall_enter = 0, first_syscall = 1;
+	type_t syscall_code = 0, max_signals = 317;
+	struct user_regs_struct *syscall_regs;
+
+	if (count <= 1)
+	{
+		printf("no command provided\n");
+		return (1);
+	}
+
+	parent = fork();
+	if (parent)
+	{
+		syscall_regs = malloc(sizeof(struct user_regs_struct));
+
+		wait(&wstatus);
+		do
+		{
+			ptrace(PTRACE_SYSCALL, parent, NULL, NULL);
+			wait(&wstatus);
+
+			if (syscall_enter && !first_syscall)
+			{
+				ptrace(PTRACE_GETREGS, parent, NULL, syscall_regs);
+
+				syscall_code = syscall_regs->orig_rax;
+				if (0 <= syscall_code && syscall_code <= max_signals)
+					fprintf(stderr, "%s\n", syscalls_64_g[syscall_code].name);
+
+			}
+			syscall_enter = !syscall_enter;
+			first_syscall = 0;
+		} while (!WIFEXITED(wstatus));
+
+		free(syscall_regs);
+	}
+	else
+	{
+		ptrace(PTRACE_TRACEME, NULL, NULL, NULL);
+		raise(SIGSTOP);
+		execve(args[1], &args[1], NULL);
+	}
+
+	return (0);
+}

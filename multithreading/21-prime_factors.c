cb7a4c43f9a4a38b5af67c55a95223de65202d27
chepeniv <jose.olmos@atlasschool.com>
2025-09-25 12:00:57 -0500

    tidyed up code, added documentation

--- multithreading/21-prime_factors.c
+++ multithreading/21-prime_factors.c
@@ -1,21 +1,27 @@
 #include "list.h"
-#include <stdlib.h>
 #include <math.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 /*
  * write a functions that factorizes a number into a list of prime numbers
  *
  * prototype: `list_t *prime_factors(char const *s);`
  *     `s` is the string representation of of the number to factorize
- * 	   this number will be positive and fit into an `unsigned long`
+ *     this number will be positive and fit into an `unsigned long`
  *
  * code will be compiled with the file `list.c`
  * **note**: this task does not require multithreading and will not be linked
  * to the `pthread` library
  */
 
-void list_add_pfactor(list_t *list, unsigned long number)
+/**
+ * list_add_pfactor - adds a given number to the given list
+ * @list: list to append the number to
+ * @number: number to handle and pass to list_add()
+ */
+
+void
+list_add_pfactor(list_t *list, unsigned long number)
 {
 	unsigned long *content;
 
@@ -25,13 +31,15 @@ void list_add_pfactor(list_t *list, unsigned long number)
 }
 
 /**
- * prime_factors -
- * @s:
+ * prime_factors - find all prime factors of a given number and add these to a
+ * list
+ * @s: the string to interpret as an `unsigned long` to be prime factored
  *
- * Return: list_t
+ * Return: resultant list with all primes of the given `s`
  */
 
-list_t *prime_factors(char const *s)
+list_t *
+prime_factors(char const *s)
 {
 	list_t *prime_factor_list;
 	unsigned long number, sqrt_limit, dividend, divisor;
@@ -44,8 +52,7 @@ list_t *prime_factors(char const *s)
 	dividend = number;
 	divisor = 2;
 
-	do
-	{
+	do {
 		if (dividend % divisor == 0)
 		{
 			dividend /= divisor;
@@ -53,7 +60,7 @@ list_t *prime_factors(char const *s)
 		}
 		else
 		{
-			if (divisor ==  2)
+			if (divisor == 2)
 				divisor++;
 			else
 				divisor += 2;
